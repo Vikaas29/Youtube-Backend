f81321983f017a94947b78b1af902b95e20780a9
Vikaas29 <vikas4644@gmail.com>
2025-01-07 21:04:18 +0530

    -

--- models/commentModel.js
+++ models/commentModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = mongoose.Schema({
     "commentData":{type:String,required:true},
     "userName":{type:String,required:true},
-    "email":{type:String,required:true,unique:true},
+    "email":{type:String,required:true},
     "videoId":{type:String,required:true},
 });
 
